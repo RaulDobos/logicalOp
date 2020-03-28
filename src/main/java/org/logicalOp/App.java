0f4dca85a9e845b0a6ea23c0809464c90dde7ded
RaulDobos <raulpriam@yahoo.com>
2020-03-28 19:24:09 +0200

    Initial commit

---
+++ src/main/java/org/logicalOp/App.java
@@ -0,0 +1,13 @@
+package org.logicalOp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
